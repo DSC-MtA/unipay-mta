57ea130173ad9aa84fff5066583cd895ff021145
syoon2 <syoon2@mta.ca>
2020-02-06 21:41:41 -0400

    Finish a button

--- lib/personSelector.dart
+++ lib/personSelector.dart
@@ -10,11 +10,30 @@ class addPersonButton extends StatelessWidget
     @override
     Widget build(BuildContext context)
     {
-        return new RawMaterialButton(
+        return RawMaterialButton(
             fillColor: Color.fromRGBO(137, 40, 52, 0.75),
             splashColor: Color.fromRGBO(137, 40, 52, 1.0),
-            child: Text("+"),
+            child:Padding(
+                padding: const EdgeInsets.symmetric(
+                    vertical: 8.0,
+                    horizontal: 20.0
+                ),
+                child:Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: const <Widget>[
+                        Icon(
+                            Icons.supervisor_account,
+                        ),
+                        SizedBox(width: 6.0,),
+                        Text("Add Member",style:TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                        ),
+                    ],
+                ),
+            ),
             onPressed: onPressed,
+            shape: StadiumBorder(),
         );
     }
 }
