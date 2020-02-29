610acffd10045afa5245fdc8d6d8436eb2c0c8e6
Graeme Zinck <graeme.zinck@gmail.com>
2020-02-29 13:36:57 -0400

    Refactored for web compatibility

--- lib/preview_bill/tip_selector.dart
+++ lib/preview_bill/tip_selector.dart
@@ -30,11 +30,48 @@ class TipSelector extends StatefulWidget {
 /// The state for the widget that selects tips on a bill.
 class _TipSelectorState extends State<TipSelector> {
   TextEditingController _tipTextController = TextEditingController();
+  var cursorPos = new TextSelection.fromPosition(new TextPosition(offset: 2));
+
+  /// Updates the tip amount to the amount given (in cents).
+  ///
+  /// Specifically, it calls the parent widget's function changeTip and
+  /// updates the local text to reflect the change.
+  void updateTip(int tip) {
+    widget.changeTip(tip);
+    _tipTextController.text = '\$  ${formatMoney(tip)}';
+  }
+
+  @override
+  void initState() {
+    // Sets the text to be the $ value passed as a property
+    // (must convert to a string format first).
+    _tipTextController.text = '\$  ${formatMoney(widget.tipAmount)}';
+
+    // When the text changes for tip selection, we do something
+    _tipTextController.addListener(() {
+      // Get the tip amount and update it
+      var tip = extractInt(_tipTextController.text);
+      widget.changeTip(tip);
+
+      // Determine what the text should be and where the cursor should be.
+      // We keep the cursor at the end of the line, so we have the offset to
+      // equal the length of the text string
+      var newText = '\$  ${formatMoney(tip)}';
+      var cursorPos = new TextSelection.fromPosition(
+          new TextPosition(offset: newText.length));
+
+      // We update the tip text controller with the new text and selection
+      _tipTextController.value = _tipTextController.value.copyWith(
+        text: newText,
+        composing: TextRange.empty,
+        selection: cursorPos,
+      );
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    // Update the tip amount to what is passed in as a parameter (tipAmount).
-    _tipTextController.text = '\$  ${formatMoney(widget.tipAmount)}';
     return Container(
       padding: EdgeInsets.fromLTRB(20, 0, 20, 20),
       child: Card(
@@ -53,13 +90,6 @@ class _TipSelectorState extends State<TipSelector> {
                     child: TextField(
                       textAlign: TextAlign.end,
                       keyboardType: TextInputType.numberWithOptions(),
-                      // On change the input, get the integer representing the
-                      // tip amount and tell the parent widget that the tip
-                      // changed. This will cause the children widgets to
-                      // refresh their data because setState() is called.
-                      onChanged: (String value) async {
-                        widget.changeTip(extractInt(value));
-                      },
                       controller: _tipTextController,
                     ),
                   ),
@@ -70,21 +100,21 @@ class _TipSelectorState extends State<TipSelector> {
                 children: <Widget>[
                   FlatButton(
                     child: const Text('10%'),
-                    // Set the tip to subtotal / 10 (integer division).
-                    onPressed: () =>
-                        widget.changeTip(widget.billSubtotal ~/ 10),
+                    // Set the tip to 10% (integer division)
+                    onPressed: () => updateTip(widget.billSubtotal ~/ 10),
                   ),
                   FlatButton(
                     child: const Text('15%'),
-                    onPressed: () =>
-                        widget.changeTip(widget.billSubtotal * 3 ~/ 20),
+                    // Set the tip to 15% (integer division)
+                    onPressed: () => updateTip(widget.billSubtotal * 3 ~/ 20),
                   ),
                   RaisedButton(
                     child: const Text(
                       '20%',
                       style: TextStyle(color: Colors.white),
                     ),
-                    onPressed: () => widget.changeTip(widget.billSubtotal ~/ 5),
+                    // Set the tip to 20% (integer division)
+                    onPressed: () => updateTip(widget.billSubtotal ~/ 5),
                     color: Theme.of(context).primaryColor,
                   ),
                 ],
